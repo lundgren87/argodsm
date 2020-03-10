282611706d4ce082d5f351ae98ffadb4e32f3ded
lundgren87 <lundgren87@gmail.com>
2020-03-10 08:51:01 +0000

    Fixes and improvements to selective coherence
    Fixes a bug with unguarded access to globalSharers.
    Ensure to poke the MPI system after each selective coherence
    operation. Some additional code cleanup.

--- src/backend/mpi/coherence.cpp
+++ src/backend/mpi/coherence.cpp
@@ -12,11 +12,15 @@
 extern control_data *cacheControl;
 extern unsigned long *globalSharers;
 extern pthread_mutex_t cachemutex;
+extern pthread_mutex_t wbmutex;
 extern sem_t ibsem;
 extern void * startAddr;
 extern char * barwindowsused;
 extern MPI_Win *globalDataWindow;
+extern MPI_Win sharerWindow;
 extern argo_statistics stats;
+extern argo_byte *touchedcache;
+extern MPI_Comm workcomm;
 
 static const unsigned int pagesize = 4096;
 
@@ -27,43 +31,44 @@ static const unsigned int pagesize = 4096;
  */
 void selective_si(void *addr, size_t size){
     unsigned long i, p, glob_addr, id, homenode, homenode_offset, classidx, idx;
-    double t1, t2;
-    double cachet1, cachet2;
+    double t1, t2, cachet1, cachet2;
 
     if(size == 0){
         return;
     }
 
+    id = 1 << getID();	
     char *address = (char*)addr;
     unsigned long lineindex_start =  (unsigned long)((unsigned long)(address) - (unsigned long)(startAddr));
     unsigned long lineindex_end = lineindex_start + size - 1;
     lineindex_start/=(pagesize*CACHELINE);
     lineindex_end/=(pagesize*CACHELINE);
 
+	/* Lock relevant mutexes. Start statistics timekeeping */
     t1 = MPI_Wtime();
-    /*
-     *Get the offset from the start of the global address space, lets call this 'global address'
-     */
     cachet1 = MPI_Wtime();
     pthread_mutex_lock(&cachemutex);
     cachet2 = MPI_Wtime();
     stats.cachemutextime += cachet2-cachet1;
     stats.cachemutextime_ssi += cachet2-cachet1;
+	pthread_mutex_lock(&wbmutex);
     sem_wait(&ibsem);
+
+	/* Iterate over all pages to selectively invalidate */
     for(p = lineindex_start; p <= lineindex_end; p++, address+=pagesize){
         glob_addr =  (unsigned long)((unsigned long)(address) - (unsigned long)(startAddr));	
         glob_addr/=(pagesize*CACHELINE);
         glob_addr*=(pagesize*CACHELINE);
 
-        id = 1 << getID();	
         homenode = getHomenode(glob_addr);
         homenode_offset = getOffset(glob_addr);
         classidx = get_classification_index(glob_addr);
         idx = getCacheIndex(glob_addr);
 
+		/* If the page is dirty, downgrade it */
         argo_byte dirty = cacheControl[idx].dirty;
         if(dirty == DIRTY){
-            /**@todo should only write back this page*/
+            mprotect((char*)startAddr + glob_addr, pagesize*CACHELINE, PROT_READ);
             cacheControl[idx].dirty = CLEAN;
             for(i = 0; i <CACHELINE; i++){
                 storepageDIFF(idx+i,glob_addr+pagesize*i);
@@ -71,34 +76,44 @@ void selective_si(void *addr, size_t size){
 
         }
 
-        /* TODO: This part causes stalling on MPI_Win_Unlock()
-         * in loadcacheline or prefetchcacheline functions */
-//        if(
-//        // node is single writer
-//        (globalSharers[classidx+1]==id)
-//        ||
-//        // No writer and assert that the node is a sharer
-//        ((globalSharers[classidx+1]==0) && ((globalSharers[classidx]&id)==id))
-//        ){
-//            //nothing - we keep the pages, SD is done in flushWB
-//        }
-//        else{ //multiple writer or SO
+		/* Optimization to keep pages in cache if they do not
+		 * need to be invalidated. */
+		MPI_Win_lock(MPI_LOCK_SHARED, getID(), 0, sharerWindow);
+        if(
+        // node is single writer
+        (globalSharers[classidx+1]==id)
+        ||
+        // No writer and assert that the node is a sharer
+        ((globalSharers[classidx+1]==0) && ((globalSharers[classidx]&id)==id))
+        ){
+			MPI_Win_unlock(getID(), sharerWindow);
+			touchedcache[idx]=1;
+            //nothing - we keep the pages, SD is done in flushWB
+        }
+        else{ //multiple writer or SO, invalidate the page
+			MPI_Win_unlock(getID(), sharerWindow);
             cacheControl[idx].dirty=CLEAN;
             cacheControl[idx].state = INVALID;
+			touchedcache[idx]=0;
             mprotect((char*)startAddr + glob_addr, pagesize*CACHELINE, PROT_NONE);
-//        }	
+        }	
     }
+	// Make sure to sync writebacks
+	for(i = 0; i < argo_get_nodes(); i++){
+		if(barwindowsused[i] == 1){
+			MPI_Win_unlock(i, globalDataWindow[i]); //Sync write backs
+			barwindowsused[i] = 0;
+		}
+	}
 
-    // Make sure to sync writebacks
-    for(i = 0; i < argo_get_nodes(); i++){
-        if(barwindowsused[i] == 1){
-            MPI_Win_unlock(i, globalDataWindow[i]); //Sync write backs
-            barwindowsused[i] = 0;
-        }
-    }
     t2 = MPI_Wtime();
     stats.ssitime += t2-t1;
+	/* Poke the MPI system to force progress */
+	int flag;
+	MPI_Iprobe(MPI_ANY_SOURCE,MPI_ANY_TAG,workcomm,&flag,MPI_STATUS_IGNORE);
+	/* Release relevant mutexes */
     sem_post(&ibsem);
+	pthread_mutex_unlock(&wbmutex);
     pthread_mutex_unlock(&cachemutex);
 }
 
@@ -110,8 +125,7 @@ void selective_si(void *addr, size_t size){
  */
 void selective_sd(void *addr, size_t size){
     unsigned long i, p ,glob_addr, idx;
-    double t1, t2;
-    double cachet1, cachet2;
+    double t1, t2, cachet1, cachet2;
 
     if(size == 0){
         return;
@@ -124,16 +138,18 @@ void selective_sd(void *addr, size_t size){
     lineindex_start/=(pagesize*CACHELINE);
     lineindex_end/=(pagesize*CACHELINE);
 
+
+	/* Lock relevant mutexes. Start statistics timekeeping */
     t1 = MPI_Wtime();
-    // Ensure exclusive cache access
     cachet1 = MPI_Wtime();
     pthread_mutex_lock(&cachemutex);
     cachet2 = MPI_Wtime();
     stats.cachemutextime += cachet2-cachet1;
     stats.cachemutextime_ssd += cachet2-cachet1;
+	pthread_mutex_lock(&wbmutex);
     sem_wait(&ibsem);
 
-    // Iterate over each page to self-downgrade
+	/* Iterate over all pages to selectively downgrade */
     for(p = lineindex_start; p <= lineindex_end; p++, address+=pagesize){
         // Get the offset from the start of the global address space
         glob_addr =  (unsigned long)((unsigned long)(address) - (unsigned long)(startAddr));	
@@ -142,7 +158,7 @@ void selective_sd(void *addr, size_t size){
 
         idx = getCacheIndex(glob_addr);
 
-        // Write back this page if it is DIRTY and set it to CLEAN
+        // Write back this page if it is DIRTY
         argo_byte dirty = cacheControl[idx].dirty;
         if(dirty == DIRTY){
             mprotect((char*)startAddr + glob_addr, pagesize*CACHELINE, PROT_READ);
@@ -152,18 +168,22 @@ void selective_sd(void *addr, size_t size){
             }
         }
     }
+	// Make sure to sync writebacks
+	for(i = 0; i < argo_get_nodes(); i++){
+		if(barwindowsused[i] == 1){
+			MPI_Win_unlock(i, globalDataWindow[i]); //Sync write backs
+			barwindowsused[i] = 0;
+		}
+	}
 
-    // Make sure to sync writebacks
-    for(i = 0; i < argo_get_nodes(); i++){
-        if(barwindowsused[i] == 1){
-            MPI_Win_unlock(i, globalDataWindow[i]); //Sync write backs
-            barwindowsused[i] = 0;
-        }
-    }
     t2 = MPI_Wtime();
     stats.ssdtime += t2-t1;
-    // Release exclusive cache access
-    sem_post(&ibsem);
+	/* Poke the MPI system to force progress */
+	int flag;
+	MPI_Iprobe(MPI_ANY_SOURCE,MPI_ANY_TAG,workcomm,&flag,MPI_STATUS_IGNORE);
+	/* Release relevant mutexes */
+	sem_post(&ibsem);
+	pthread_mutex_unlock(&wbmutex);
     pthread_mutex_unlock(&cachemutex);
 }
 
@@ -177,8 +197,7 @@ void selective_sd(void *addr, size_t size){
  */
 void selective_sd_region(void *addr, size_t size){
     unsigned long i, p, homenode, offset, glob_addr, idx, regionstart, regionend;
-    double t1, t2;
-    double cachet1, cachet2;
+    double t1, t2, cachet1, cachet2;
 
     if(size == 0){
         return;
@@ -193,13 +212,14 @@ void selective_sd_region(void *addr, size_t size){
     lineindex_start/=(pagesize*CACHELINE);
     lineindex_end/=(pagesize*CACHELINE);
 
+	/* Lock relevant mutexes. Start statistics timekeeping */
     t1 = MPI_Wtime();
-    // Ensure exclusive cache access
     cachet1 = MPI_Wtime();
     pthread_mutex_lock(&cachemutex);
     cachet2 = MPI_Wtime();
     stats.cachemutextime += cachet2-cachet1;
     stats.cachemutextime_ssd += cachet2-cachet1;
+	pthread_mutex_lock(&wbmutex);
     sem_wait(&ibsem);
 
     // Iterate over each page to self-downgrade
@@ -218,26 +238,28 @@ void selective_sd_region(void *addr, size_t size){
         if(dirty == DIRTY){
             mprotect((char*)startAddr + glob_addr, pagesize*CACHELINE, PROT_READ);
             cacheControl[idx].dirty = CLEAN;
-
-            // If it is the first or last page and start or end are not page aligned
-            // Alternatively, if the size is smaller than pagesize
-            if( (size<pagesize) ||
+			
+            // If the size is smaller than pagesize
+			if( (size<pagesize) ||
+            	// OR,  it is the first page in the region it is not page aligned
                 (p == lineindex_start && regionstart%pagesize != 0) || 
+				// OR, it is the last page in the region and it is not page aligned
                 (p == lineindex_end && regionend%pagesize != 0)) {
                 // Create and send a diff for the page
                 for(i = 0; i <CACHELINE; i++){
                     storepageDIFF(idx+i,glob_addr+pagesize*i);
                 }
             }
-            // If the whole page is in the region we do not need a diff
+            // Else, the whole page is in our region and we do not create a diff
             else{
                 if(barwindowsused[homenode] == 0){
-		    MPI_Win_lock(MPI_LOCK_EXCLUSIVE, homenode, 0, globalDataWindow[homenode]);
-		    barwindowsused[homenode] = 1;
-	        }
+		    		MPI_Win_lock(MPI_LOCK_EXCLUSIVE, homenode, 0, globalDataWindow[homenode]);
+		    		barwindowsused[homenode] = 1;
+	        	}
                 // Send the whole page
                 MPI_Put((void*)((char*)startAddr + glob_addr), pagesize, MPI_BYTE, homenode, offset, pagesize, MPI_BYTE, globalDataWindow[homenode]);
             }
+			// TODO: Send multiple consecutive pages with one MPI_Put
         }
     }
 
@@ -250,7 +272,11 @@ void selective_sd_region(void *addr, size_t size){
     }
     t2 = MPI_Wtime();
     stats.ssdtime += t2-t1;
-    // Release exclusive cache access
+	/* Poke the MPI system to force progress */
+	int flag;
+	MPI_Iprobe(MPI_ANY_SOURCE,MPI_ANY_TAG,workcomm,&flag,MPI_STATUS_IGNORE);
+    /* Release relevant mutexes */
     sem_post(&ibsem);
+	pthread_mutex_unlock(&wbmutex);
     pthread_mutex_unlock(&cachemutex);
 }
