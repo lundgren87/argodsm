6c0761716e0b9ed7c520650be7c3bd0d79c6df6d
lundgren87 <lundgren87@gmail.com>
2020-04-17 08:31:00 +0100

    Add new unit tests for prefetching
    This commit adds two simple unit tests for loading and
    prefetching. These unit tests test access to the first and last
    byte in full memory allocation, as well as access to subsequent
    pages in an allocation where the second page accessed may already
    have been prefetched.

--- tests/prefetch.cpp
+++ tests/prefetch.cpp
@@ -24,6 +24,12 @@ constexpr std::size_t cache_size = size/8;
 namespace mem = argo::mempools;
 extern mem::global_memory_pool<>* default_global_mempool;
 
+/** @brief ArgoDSM page size */
+const unsigned int pagesize = 4096;
+
+/** @brief A random int constant */
+constexpr char c_const = 'a';
+
 /**
  * @brief Class for the gtests fixture tests. Will reset the allocators to a clean state for every test
  */
@@ -42,18 +48,46 @@ class PrefetchTest : public testing::Test {
 
 
 
+/**
+ * @brief Unittest that checks that there is no error when accessing the first byte of the allocation.
+ */
+TEST_F(PrefetchTest, FirstPage) {
+	std::size_t allocsize = default_global_mempool->available();
+	char *tmp = static_cast<char*>(collective_alloc(allocsize));
+	if(argo::node_id()==0){
+		tmp[0] = c_const;
+	}
+	argo::barrier();
+	ASSERT_EQ(c_const, tmp[0]);
+}
 
 /**
- * @brief Unittest that checks that there is no error when accessing the last page in memory and tried to prefetch the page after.
+ * @brief Unittest that checks that there is no error when accessing the last page in memory.
  */
 TEST_F(PrefetchTest, OutOfBounds) {
 	std::size_t allocsize = default_global_mempool->available();
 	char *tmp = static_cast<char*>(collective_alloc(allocsize));
 	if(argo::node_id()==0){
-		tmp[allocsize-1]=42;
+		tmp[allocsize-1] = c_const;
 	}
+	argo::barrier();
+	ASSERT_EQ(c_const, tmp[allocsize-1]);
 }
 
+/**
+ * @brief Unittest that checks that there is no error when accessing a page that may already be prefetched by a previous access.
+ */
+TEST_F(PrefetchTest, AccessPrefetched) {
+	std::size_t allocsize = default_global_mempool->available();
+	char *tmp = static_cast<char*>(collective_alloc(allocsize));
+	if(argo::node_id()==0){
+		tmp[pagesize-1] = c_const;
+		tmp[pagesize] = c_const;
+	}
+	argo::barrier();
+	ASSERT_EQ(c_const, tmp[pagesize-1]);
+	ASSERT_EQ(c_const, tmp[pagesize]);
+}
 
 /**
  * @brief The main function that runs the tests
